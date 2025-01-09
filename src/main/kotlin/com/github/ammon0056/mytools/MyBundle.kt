db44a941213f3062222488fe3ef2fff8ca9a6951
GitHub Action <action@github.com>
2025-01-09 02:46:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/ammon0056/mytools/MyBundle.kt
+++ src/main/kotlin/com/github/ammon0056/mytools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ammon0056.mytools
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ammon0056/mytools/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/ammon0056/mytools/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ammon0056.mytools.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
